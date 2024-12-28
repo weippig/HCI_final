e0b066c1da8b7f29ead0c6dedf3b646e9751a158
Helen Huang <helpig88319@gmail.com>
2024-12-28 23:23:49 +0800

    first commit

---
+++ js/scriptent.js
@@ -0,0 +1,182 @@
+const fingers = {
+    index1: 8,
+    index2: 7,
+    index3: 6,
+    index4: 5,
+    middle1: 12,
+    middle2: 11,
+    middle3: 10,
+    middle4: 9,
+    ring1: 16,
+    ring2: 15,
+    ring3: 14,
+    ring4: 13,
+    little1: 20,
+    little2: 19,
+    little3: 18,
+    little4: 17,
+    thumb1: 4,
+    thumb2: 3,
+    thumb3: 2,
+    thumb4: 1,
+    thumb5: 0
+}
+
+const finger_state = {
+    landmarks: undefined,
+    index: false,
+    middle: false,
+    ring: false,
+    little: false
+}
+
+function gesture() {
+    /*
+        0 : nothing
+        1 : index up, drawing state
+        2 : index and middle up, eraser state
+    */
+    if (finger_state.index && !finger_state.middle && !finger_state.ring && !finger_state.little) {return 1;}
+    if (finger_state.index && finger_state.middle && !finger_state.ring && !finger_state.little) {return 2;}
+    return 0;
+}
+
+function download_points(stroke_list) {
+    const a = document.createElement("a");
+    const file = stroke_list.download();
+    a.href = URL.createObjectURL(file);
+    a.download = "data.txt";
+    a.click();
+}
+
+class Point {
+    constructor(x,y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    static distance(a,b) {
+        return Math.hypot(a.x-b.x,a.y-b.y);
+    }
+}
+
+function init() {
+    const download_button = document.querySelector('#download_button')
+    const clear_button = document.querySelector('#clear_button')
+    const dl_button = document.querySelector('#dl_button')
+    const video = document.querySelector('video');
+    const canvas = document.querySelector('canvas');
+    const context = canvas.getContext('2d');
+
+    const width = canvas.width;
+    const height = canvas.height;
+
+    const erase_radius = 40.;
+
+    const draw_icon = new Image();
+    const erase_icon = new Image();
+    draw_icon.src = 'assets/draw.png';
+    erase_icon.src = 'assets/erase.png';
+
+    let stroke_list = new StrokeList();
+    let previous_pt = null;
+    download_button.onclick = () => download_points(stroke_list);
+    clear_button.onclick = () => stroke_list.clear();
+    dl_button.onclick = () => stroke_list.predict();
+
+    async function process() {
+        context.save();
+
+        // draw video stream
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+        // draw hands
+        await hands.send({image: video});
+
+        let gest = gesture();
+
+        // draw icons
+        
+        if (gest == 1) {
+            // the user is drawing
+            context.globalAlpha = 1;
+            context.drawImage(draw_icon,width-166,height-100);
+            // register point
+            index_pos = finger_state.landmarks[fingers.index1];
+            new_pt = new Point(index_pos.x*width,index_pos.y*height);
+            stroke_list.add_pt(new_pt);
+            previous_pt = new_pt;
+        } else {
+            if (previous_pt !== null) {
+                stroke_list.new_stroke();
+                previous_pt = null;
+            }
+            context.globalAlpha = 0.2;
+            context.drawImage(draw_icon,width-166,height-100);
+        }
+
+        if (gest == 2) {
+            // the user is erasing
+            context.globalAlpha = 1;
+            context.drawImage(erase_icon,width-166,height-200);
+            // register erase
+            idx = finger_state.landmarks[fingers.index1];
+            mdl = finger_state.landmarks[fingers.middle1];
+            erase_pos = new Point(width*(idx.x+mdl.x)/2.,height*(idx.y+mdl.y)/2.);
+            // filter erased points
+            stroke_list.erase(erase_pos,erase_radius);
+            // draw eraser
+            context.lineWidth = 5;
+            context.strokeStyle = 'salmon';
+            context.beginPath();
+            context.arc(erase_pos.x, erase_pos.y, erase_radius, 0, 2*Math.PI);
+            context.stroke()
+        } else {
+            context.globalAlpha = 0.2;
+            context.drawImage(erase_icon,width-166,height-200);
+        }
+        context.restore();
+
+        context.save();
+        stroke_list.draw(context);
+        context.restore();
+    }
+
+    function processHands(results) {
+        if (results.multiHandLandmarks) {
+            for (const landmarks of results.multiHandLandmarks) {
+                drawConnectors(context, landmarks, HAND_CONNECTIONS,{color: '#00FF00', lineWidth: 5});
+                drawLandmarks(context, landmarks, {color: '#FF0000', lineWidth: 2});
+
+                // update fingers state
+                finger_state.landmarks = landmarks;
+                finger_state.index = landmarks[fingers.index1].y < landmarks[fingers.index3].y;
+                finger_state.middle = landmarks[fingers.middle1].y < landmarks[fingers.middle3].y;
+                finger_state.ring = landmarks[fingers.ring1].y < landmarks[fingers.ring3].y;
+                finger_state.little = landmarks[fingers.little1].y < landmarks[fingers.little3].y;
+            }
+        }
+    }
+
+    const hands = new Hands({locateFile: (file) => {
+        return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+    }});
+    hands.setOptions({
+        selfieMode: false,
+        maxNumHands: 1,
+        minDetectionConfidence: 0.5,
+        minTrackingConfidence: 0.5,
+        modelComplexity: 1.
+    });
+    hands.onResults(processHands);
+
+    const camera = new Camera(video, {
+        onFrame: process,
+        width: 1920,
+        height: 1080
+    });
+    camera.start();
+}
+
+window.onload = init
---
+++ js/strokes.js
@@ -0,0 +1,97 @@
+class StrokeList {
+    constructor() {
+        this.stroke_list = [[]];
+    }
+
+    add_pt (pt) {
+        this.stroke_list.at(-1).push(pt)
+    }
+
+    clear () {
+        this.stroke_list = [[]];
+    }
+
+    erase (erase_pos,radius) {
+        this.filter((index,pt) => (Point.distance(erase_pos,pt) > radius))
+    }
+
+    new_stroke () {
+        if (!this.stroke_list.length || this.stroke_list.at(-1).length) {
+            this.stroke_list.push([]);
+        }
+    }
+
+    draw (context) {
+        context.lineJoin = "round";
+        context.strokeStyle = "magenta";
+        context.shadowColor = "magenta";
+        context.shadowBlur = 10;
+        context.lineWidth = 5;
+        for (const stroke of this.stroke_list) {
+            if (stroke.length) {
+                context.beginPath();
+                context.moveTo(stroke[0].x, stroke[0].y);
+                for (const pt of stroke.slice(1)) {
+                    context.lineTo(pt.x, pt.y);
+                }
+                context.stroke();
+            }
+        }
+    }
+
+    download () {
+        return new Blob([JSON.stringify(this.stroke_list.flat(), null, 2)], {type: "text/plain"});
+    }
+
+    filter (criterion) {
+        /*
+        criterion is a function (index,element) => bool which tells wether element at a given index
+        should be kept 
+        */
+        let old_stroke_list = this.stroke_list.slice();
+        this.stroke_list = [[]];
+        const n = old_stroke_list.length;
+        let count = 0
+        for (let k = 0; k < n; k++) {
+            let n_k = old_stroke_list[k].length;
+            for (let i = 0; i < n_k; i++) {
+                if (criterion(count,old_stroke_list[k][i])) {this.add_pt(old_stroke_list[k][i]);}
+                else {this.new_stroke();}
+                count++;
+            }
+            this.new_stroke();
+        }
+        this.new_stroke();
+    }
+
+    // Use a DL on the sequence to predict wether the user wanted to draw or not
+    async predict () {
+        let data = this.stroke_list.slice().flat();
+        data = data.map(x => {return [x.x,x.y];})
+        data = transformData(data);
+
+        const session = await ort.InferenceSession.create('models/lstm_X_91_48.onnx');
+        const tensor = new ort.Tensor('float32',data.flat(),[data.length,6]);
+        const feed = {'input': tensor};
+        const result = await session.run(feed);
+
+        const output = result.output.data;
+        this.filter((index,pt) => (output[index] > 0.5));
+    }
+}
+
+// Takes an array of 2D coordinates and returns an array of 6D values (vx,vy,v,ax,ay,a)
+function transformData(data) {
+    let new_data = [[0.,0.,0.,0.,0.,0.]];
+    let n = data.length;
+    for (let k = 1; k < n; k++) {
+        vx = data[k][0] - data[k-1][0];
+        vy = data[k][1] - data[k-1][1];
+        v = Math.hypot(vx,vy);
+        ax = vx - new_data.at(-1)[0];
+        ay = vy - new_data.at(-1)[1];
+        a = Math.hypot(ax,ay);
+        new_data.push([vx,vy,v,ax,ay,a]);
+    }
+    return new_data;
+}
